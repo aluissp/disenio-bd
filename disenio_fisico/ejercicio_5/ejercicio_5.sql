7711ab64a6caaca1a4b5704d05a02b871aead49b
Luis Perugachi <luisprg05@gmail.com>
2023-02-01 19:23:56 -0500

    Sql ejercicio 5 hecho

---
+++ disenio_fisico/ejercicio_5/ejercicio_5.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS ejercicio_5;
+
+CREATE DATABASE ejercicio_5;
+
+USE ejercicio_5;
+
+CREATE TABLE buildings(
+  city VARCHAR(255) NOT NULL,
+  number INT UNSIGNED NOT NULL,
+  PRIMARY KEY(city, number)
+);
+
+CREATE TABLE departments(
+  number VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  annual_budget DECIMAL(8, 2),
+  city VARCHAR(255) NOT NULL,
+  building_number INT UNSIGNED NOT NULL,
+  FOREIGN KEY(city, building_number) REFERENCES buildings(city, number) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE employees(
+  number INT UNSIGNED NOT NULL,
+  department_number VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  manager_number INT UNSIGNED NOT NULL,
+  manager_department_number VARCHAR(255) NOT NULL,
+  PRIMARY KEY (number, department_number),
+  FOREIGN KEY (department_number) REFERENCES departments(number) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (manager_number, manager_department_number) REFERENCES employees(number, department_number) ON UPDATE CASCADE ON DELETE RESTRICT
+);
